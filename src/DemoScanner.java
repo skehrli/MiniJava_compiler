b6a92722fe3b41f685f7dcc8e9451db4feda333e
Sascha Kehrli <skehrli@ethz.ch>
2023-10-07 17:26:51 -0700

    wrote main MiniJava file to read from file

--- src/DemoScanner.java
+++ src/DemoScanner.java
@@ -12,7 +12,7 @@ public class DemoScanner {
             Reader in = new BufferedReader(new InputStreamReader(System.in));
             scanner s = new scanner(in, sf);
             Symbol t = s.next_token();
-            while (t.sym != sym.EOF) { 
+            while (t.sym != sym.EOF) {
                 // print each token that we scan
                 System.out.print(s.symbolToString(t) + " ");
                 t = s.next_token();
@@ -20,12 +20,10 @@ public class DemoScanner {
         } catch (Exception e) {
             // yuck: some kind of error in the compiler implementation
             // that we're not expecting (a bug!)
-            System.err.println("Unexpected internal compiler error: " + 
-                        e.toString());
+            System.err.println("Unexpected internal compiler error: " +
+                    e.toString());
             // print out a stack dump
             e.printStackTrace();
         }
-   }
+    }
 }
-
-
---
+++ src/MiniJava.java
@@ -0,0 +1,31 @@
+import Scanner.*;
+import Parser.sym;
+import java_cup.runtime.Symbol;
+import java_cup.runtime.ComplexSymbolFactory;
+import java.io.*;
+
+public class MiniJava {
+  public static void main(String[] args) {
+    try {
+        // create a scanner on the input file
+        ComplexSymbolFactory sf = new ComplexSymbolFactory();
+        String filename = args[1];
+        FileReader fileReader = new FileReader(filename);
+        Reader in = new BufferedReader(fileReader);
+        scanner s = new scanner(in, sf);
+        Symbol t = s.next_token();
+        while (t.sym != sym.EOF) { 
+            // print each token that we scan
+            System.out.print(s.symbolToString(t) + " ");
+            t = s.next_token();
+        }
+    } catch (Exception e) {
+        // yuck: some kind of error in the compiler implementation
+        // that we're not expecting (a bug!)
+        System.err.println("Unexpected internal compiler error: " + 
+                    e.toString());
+        // print out a stack dump
+        e.printStackTrace();
+    }
+  }
+}
