5844d565902f16fcd73f5f6e561d28b524cf22d1
markpock <markpock@uw.edu>
2023-10-11 16:15:01 -0700

    Fixed a resource leak

--- test/junit/TestScanner.java
+++ test/junit/TestScanner.java
@@ -84,8 +84,8 @@ public class TestScanner {
     }
 
     private Stream<String> fileNames(String dir) {
-        try { return Files.walk(Paths.get(dir))
-                    .map(Path::toFile)
+        try (Stream<Path> paths = Files.walk(Paths.get(dir))) {
+            return paths.map(Path::toFile)
                     .filter(File::isFile)
                     .map(File::getName)
                     .map(x -> x.substring(0, x.length() - 5));
