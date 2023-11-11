07e5433e6d5ddfff70d11acd73792353526605d0
markpock <markpock@uw.edu>
2023-11-10 21:21:44 -0800

    Deleted unused field from ASTNode.java

--- src/AST/ASTNode.java
+++ src/AST/ASTNode.java
@@ -1,14 +1,10 @@
 package AST;
 
-import AST.Visitor.Visitor;
-import Semantics.Bottom;
-import Semantics.Type;
 import java_cup.runtime.ComplexSymbolFactory.Location;
 
 abstract public class ASTNode {
   // Line number in source file.
   public final int line_number;
-  Type type = Bottom.get();
 
   // Constructor
   public ASTNode(Location pos) {
